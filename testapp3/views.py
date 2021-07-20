3b53d67fdf68c6aa3da272818b560f9fbf45b02c
awerty_naxis <legendarno25>
2021-07-20 22:30:57 +0300

    test serializers, views, urls

--- testapp3/views.py
+++ testapp3/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
 
+from .models import Client, Master
+from .serializers import ClientSerializer, MasterSerializer
 # Create your views here.
+
+
+class ClientView(generics.ListCreateAPIView):
+    serializer_class = ClientSerializer
+    queryset = Client.objects.all()
+
+
+class MasterView(generics.ListCreateAPIView):
+    serializer_class = MasterSerializer
+    queryset = Master.objects.all()
+
+
+list_client_view = ClientView.as_view()
+list_master_view = MasterView.as_view()
+
