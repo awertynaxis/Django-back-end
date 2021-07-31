5c0651e74a24fef95e81b3a596cf62af19e753fe
Alexei Glon <alexglon@outlook.com>
2021-08-01 00:01:28 +0300

    Schedule slots update implementation

--- order/views.py
+++ order/views.py
@@ -1,8 +1,14 @@
-from rest_framework import generics
+from datetime import datetime, timedelta
+from django.utils.dateparse import parse_datetime
+from rest_framework import generics, status
+from rest_framework.response import Response
 
+from client.models import Client
 from order.models import Order
 from order.serializers import OrderSerializer, CreateOrderSerializer, OrderForMasterSerializer
 from order.filters import OrderClientFilter, OrderMasterFilter
+from schedule.models import Schedule
+from schedule.serializers import ScheduleSerializer
 
 
 class OrderListView(generics.ListCreateAPIView):
@@ -21,8 +27,41 @@ class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 class OrderCreateView(generics.CreateAPIView):
     """Creates an order."""
+    model = Order
     serializer_class = CreateOrderSerializer
-    queryset = Order.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        client_telegram_id = self.request.data['client_telegram_id']
+        client = Client.objects.get(client_telegram_id=client_telegram_id)
+        serializer.save(client=client)
+        self.schedule_update(serializer.data['id'])
+
+    def schedule_update(self, order):
+        duration = self.request.data['duration']
+        master = self.request.data['master']
+        start_slot = parse_datetime(self.request.data['start_datetime_slot'])
+
+        # for offset in range(duration):
+        #     # get schedule slot; with each iteration of offset, we gain 1 hour
+        #     # edit current slot's order ID
+        #     # save slot
+        #     pass
+
+        slots = [
+            Schedule.objects.get(master_id=master,
+                                 datetime_slot=(start_slot + timedelta(hours=offset)).strftime("%Y-%m-%dT%H:%M:%SZ"))
+            for offset in range(duration)
+        ]
+        for slot in slots:
+            slot.order_id = order
+        Schedule.objects.bulk_update(slots, ['order_id'])
 
 
 class OrderMasterListView(generics.ListAPIView):
