f26fd7f08f4faf62e8dc778fae1f29ad9a0a5f23
Alexei Glon <alexglon@outlook.com>
2021-08-01 05:23:59 +0300

    Docstrings and comments added

--- order/views.py
+++ order/views.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 from django.utils.dateparse import parse_datetime
 from rest_framework import generics, status
 from rest_framework.response import Response
@@ -8,7 +8,6 @@ from order.models import Order
 from order.serializers import OrderSerializer, CreateOrderSerializer, OrderForMasterSerializer
 from order.filters import OrderClientFilter, OrderMasterFilter
 from schedule.models import Schedule
-from schedule.serializers import ScheduleSerializer
 
 
 class OrderListView(generics.ListCreateAPIView):
@@ -26,7 +25,7 @@ class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class OrderCreateView(generics.CreateAPIView):
-    """Creates an order."""
+    """Creates an order and ties it to corresponding schedule slots."""
     model = Order
     serializer_class = CreateOrderSerializer
 
@@ -38,29 +37,33 @@ class OrderCreateView(generics.CreateAPIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def perform_create(self, serializer):
+        """Gets an internal ID of the Telegram user making the order, adds it to the order entry and saves the entry."""
         client_telegram_id = self.request.data['client_telegram_id']
         client = Client.objects.get(client_telegram_id=client_telegram_id)
         serializer.save(client=client)
         self.schedule_update(serializer.data['id'])
 
     def schedule_update(self, order):
+        """Updates corresponding schedule slots with the order booked on these slots."""
+        # all these fields are present in a JSON sent to the endpoint
         duration = self.request.data['duration']
         master = self.request.data['master']
         start_slot = parse_datetime(self.request.data['start_datetime_slot'])
 
-        # for offset in range(duration):
-        #     # get schedule slot; with each iteration of offset, we gain 1 hour
-        #     # edit current slot's order ID
-        #     # save slot
-        #     pass
-
+        # creating a list of all slots corresponding to the order
         slots = [
             Schedule.objects.get(master_id=master,
+                                 # corresponding timeslots are calculated by taking the timestamp of the beginning
+                                 # of the order and adding an offset (as we iterate through `for` condition below)
+                                 # result datetime object is turned back into a str as get() method accepts strings only
+                                 # TODO: will this work with timezones other than UTC+0?
                                  datetime_slot=(start_slot + timedelta(hours=offset)).strftime("%Y-%m-%dT%H:%M:%SZ"))
             for offset in range(duration)
         ]
+        # editing `order` fields of each corresponding slot
         for slot in slots:
             slot.order_id = order
+        # bulk_update() lets us update all corresponding slots at once
         Schedule.objects.bulk_update(slots, ['order_id'])
 
 
